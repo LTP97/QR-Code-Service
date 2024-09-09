1cb00e7e236c2e8967edca35a5c42090c6e8ff12
LTP97 <l.preimel@gmx.net>
2024-09-09 13:50:50 +0200

    first commit

---
+++ src/main/java/com/example/qrCodeServiceV1/QrCodeServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.example.qrCodeServiceV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeServiceV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qrCodeServiceV1/QrCodeServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrCodeServiceV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
